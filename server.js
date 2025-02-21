47c729fa9196e85539006035da1c20d11997c55d
balarama756 <balaramak86@gmail.com>
2025-02-21 21:27:49 +0530

    commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 // Debug Middleware: Log Incoming Requests
 app.use((req, res, next) => {
